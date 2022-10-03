bc8d8d57ee996ccba9614b6169eca1cdb8539790
cc959 <59772837+cc959@users.noreply.github.com>
2022-10-03 20:24:47 +0200

    Cleaned up code a bit

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-args = -lX11 -lGL -lGLEW -lsfml-audio -lsfml-graphics -lsfml-network -lsfml-system -lsfml-window -std=c++20 -O3
+args = -lX11 -std=c++20 -O3
 
 CXX = gcc # Flag for implicit rules
 CXXFLAGS = -g -I ./ $(args) -std=c++20 # Flag for implicit rules. Turn on debug info
 
 objects = main.o
 
-build: copy $(objects)
+build: $(objects)
 	g++ $(objects) $(args) -o ./Build/main
 
 rebuild: clean build
@@ -15,9 +15,6 @@ rebuild: clean build
 # In the case of the first target, foo.o, the target-pattern matches foo.o and sets the "stem" to be "foo".
 # It then replaces the '%' in prereq-patterns with that stem
 $(objects): %.o: %.cpp
-
-copy:
-	rsync --archive --delete ./res ./Build
 
 clean:
 	rm -f *.o all
--- main.cpp
+++ main.cpp
@@ -12,91 +12,96 @@ const int Rand = 10000; // 10ms
 // fast pseudorandom numbers https://www.shadertoy.com/view/ttc3zr
 uint murmurHash(uint src)
 {
-    const uint M = 0x5bd1e995u;
-    uint h = 1190494759u;
-    src *= M;
-    src ^= src >> 24u;
-    src *= M;
-    h *= M;
-    h ^= src;
-    h ^= h >> 13u;
-    h *= M;
-    h ^= h >> 15u;
-    return h;
+	const uint M = 0x5bd1e995u;
+	uint h = 1190494759u;
+	src *= M;
+	src ^= src >> 24u;
+	src *= M;
+	h *= M;
+	h ^= src;
+	h ^= h >> 13u;
+	h *= M;
+	h ^= h >> 15u;
+	return h;
 }
 
 void emit(int fd, int type, int code, int val)
 {
-    struct input_event ie;
+	struct input_event ie;
 
-    memset(&ie, 0, sizeof(struct input_event));
+	memset(&ie, 0, sizeof(struct input_event));
 
-    ie.type = type;
-    ie.code = code;
-    ie.value = val;
-    /* timestamp values below are ignored */
-    ie.time.tv_sec = 0;
-    ie.time.tv_usec = 0;
+	ie.type = type;
+	ie.code = code;
+	ie.value = val;
+	/* timestamp values below are ignored */
+	ie.time.tv_sec = 0;
+	ie.time.tv_usec = 0;
 
-    write(fd, &ie, sizeof(ie));
+	write(fd, &ie, sizeof(ie));
 }
 
 void click(int fd)
 {
-    emit(fd, EV_KEY, BTN_LEFT, 1);
-    emit(fd, EV_SYN, SYN_REPORT, 0);
+	emit(fd, EV_KEY, BTN_LEFT, 1);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
 
-    emit(fd, EV_KEY, BTN_LEFT, 0);
-    emit(fd, EV_SYN, SYN_REPORT, 0);
+	emit(fd, EV_KEY, BTN_LEFT, 0);
+	emit(fd, EV_SYN, SYN_REPORT, 0);
 }
 
 int main()
 {
-    Display *display = XOpenDisplay(0);
+	Display *display = XOpenDisplay(0);
 
-    struct uinput_setup usetup;
+	struct uinput_setup usetup;
 
-    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+	int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 
-    ioctl(fd, UI_SET_EVBIT, EV_KEY);
-    ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
+	ioctl(fd, UI_SET_EVBIT, EV_KEY);
+	ioctl(fd, UI_SET_KEYBIT, BTN_LEFT);
 
-    ioctl(fd, UI_SET_EVBIT, EV_REL);
-    ioctl(fd, UI_SET_RELBIT, REL_X);
-    ioctl(fd, UI_SET_RELBIT, REL_Y);
+	ioctl(fd, UI_SET_EVBIT, EV_REL);
+	ioctl(fd, UI_SET_RELBIT, REL_X);
+	ioctl(fd, UI_SET_RELBIT, REL_Y);
 
-    memset(&usetup, 0, sizeof(usetup));
-    usetup.id.bustype = BUS_USB;
-    usetup.id.vendor = 69;   /* sample vendor */
-    usetup.id.product = 420; /* sample product */
-    strcpy(usetup.name, "SpamBoi");
+	memset(&usetup, 0, sizeof(usetup));
+	usetup.id.bustype = BUS_USB;
+	usetup.id.vendor = 69;	 /* sample vendor */
+	usetup.id.product = 420; /* sample product */
+	strcpy(usetup.name, "SpamBoi");
 
-    ioctl(fd, UI_DEV_SETUP, &usetup);
-    ioctl(fd, UI_DEV_CREATE);
+	ioctl(fd, UI_DEV_SETUP, &usetup);
+	ioctl(fd, UI_DEV_CREATE);
 
-    int t = 0;
-    /* event loop */
-    while (1)
-    {
-        char keys[32];
-        XQueryKeymap(display, keys);
+	int t = 0;
+	/* event loop */
+	while (1)
+	{
+		char keys[32];
+		XQueryKeymap(display, keys);
 
-        // check if f6 pressed
-        if (keys[9] & 1)
-        {
-            // click(fd);
-            cerr << "Clicked!\n";
-        }
+		// to check what the key you want is
+		// for (int i = 0; i < 32; i++)
+		// 	if (keys[i])
+		// 		cout << i << " " << keys[i] << "\n";
 
-        int r = murmurHash(t++) % (2 * Rand) - Rand;
+		// check if f6 pressed
+		if (keys[9] & 1)
+		{
+			click(fd);
+			cout << "Clicked!\n";
+		}
 
-        usleep((1000000 / CPS) + r);
-    }
+		int r = murmurHash(t++) % (2 * Rand) - Rand;
 
-    sleep(1);
+		usleep((1000000 / CPS) + r);
+	}
 
-    ioctl(fd, UI_DEV_DESTROY);
-    close(fd);
+	sleep(1);
 
-    return 0;
+	ioctl(fd, UI_DEV_DESTROY);
+	close(fd);
+
+	return 0;
 }
